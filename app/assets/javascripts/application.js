13c6c27bd759b817d48a20a4a0df3558a76827fb
Spundun Bhatt <spundun@gmail.com>
2013-09-08 13:17:17 -0700

    rails g ember:bootstrap --javascript-engine coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,18 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require emblem_coffee_rails_starter_kit
+
+// for more details see: http://emberjs.com/guides/application/
+EmblemCoffeeEmberjsRailsStarterKit = Ember.Application.create();
+
+// Spundun:
+// `rails g ember:bootstrap` bumped the following require_tree command below a line of javascript.
+// This means that require_tree here is not going to work because it's after a javascript line
+// But that's ok because we don't need it.
+// If you wish to use it then paste it right below the other require statements
 //= require_tree .
---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,5 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+EmblemCoffeeEmberjsRailsStarterKit.Router.map ()->
+  # @resource('posts')
+
