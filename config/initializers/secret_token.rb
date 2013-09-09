4514cd4dfbf508bfbcdfcc6723d589efe9124861
Spundun Bhatt <spundun@gmail.com>
2013-09-08 23:04:43 -0700

    rails new emblem-coffee-emberjs-rails-starter-kit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmblemCoffeeEmberjsRailsStarterKit::Application.config.secret_key_base = 'b049d62520fd4a6dec230f41c8ec9478f1accb880e0b41c5b0b532d0c5623d98a495c7c2d0878c2be2c18b34ba75a7d2541313117c1aad23a965376cebefe403'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmblemCoffeeEmberjsRailsStarterKit::Application.config.session_store :cookie_store, key: '_emblem-coffee-emberjs-rails-starter-kit_session'
